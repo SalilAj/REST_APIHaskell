e127b1b5c587e763abf91174d8ea47834c763bcd
Salil Ajgaonkar <ajgaonkar.salil@gmail.com>
2017-12-15 02:02:40 +0000

    implemented Queue

--- master.py
+++ master.py
@@ -1,5 +1,7 @@
 import requests
+import Queue
 
+jobQueue = Queue.Queue()
 def pullRepository():
 
 	tokenFile = open('gittoken', 'r')
@@ -8,9 +10,11 @@ def pullRepository():
 	resp = requests.get('https://api.github.com/repos/avast-tl/retdec/commits', params=payload)
 	for i in resp.json():
 		print i['sha']
-		print '@@@@@@@@@@@@@@@@@@'
+		jobQueue.put(i['sha'])
+		print jobQueue.qsize()
 
 def main():
 	pullRepository()
+
 
 if __name__ == "__main__": main()
