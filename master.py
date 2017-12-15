6b840dba9a65b555e0cd0b85d6098d7b4dfda45b
Salil Ajgaonkar <ajgaonkar.salil@gmail.com>
2017-12-15 01:31:33 +0000

    Got the sha of all the commits via gihub API

--- master.py
+++ master.py
@@ -3,11 +3,14 @@ import requests
 def pullRepository():
 
 	tokenFile = open('gittoken', 'r')
-	print tokenFile.read()
+	token =  tokenFile.read()
+	payload = {'access_token': token}
+	resp = requests.get('https://api.github.com/repos/avast-tl/retdec/commits', params=payload)
+	for i in resp.json():
+		print i['sha']
+		print '@@@@@@@@@@@@@@@@@@'
 
 def main():
 	pullRepository()
-
-
 
 if __name__ == "__main__": main()
